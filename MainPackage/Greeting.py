3655ddf0903131fc2877ec226d6f220b2c86fcdc
Md Azad Hossain Tutul <58014179+mdazadhossain95@users.noreply.github.com>
2020-07-20 01:54:42 +0600

    add new package

---
+++ MainPackage/Greeting.py
@@ -0,0 +1,99 @@
+greeting = "Hello"
+# name = input("please enter your name ")
+# print(greeting + ' ' + name)
+
+splitString = "This string has been\nsplit over\nseveral"
+
+tabbedString = "1\t2\t3\n4\t5\t"
+print(tabbedString)
+
+print('The pet shop owner said "No, no,\'e\'s uh,.....he\s resting"')
+print("The pet shop owner said \"No , no , 'e's uh,.....he's resting\"")
+
+anotherSplitString = """This string has been
+split over
+several lines"""
+print(anotherSplitString)
+
+print('''The pet shop owner said "No , no , 'e's uh,.....he's resting''')
+print("""The pet shop owner said "No , no , 'e's uh,.....he's resting""")
+
+gretting = "Tutul"
+_myname = "azad"
+azad5 = "good"
+Azad_was_01 = "hello"
+Greeting = "There"
+print(Azad_was_01 + ' ' + gretting)
+print("======")
+
+
+a= 12
+b = 3
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a%b)
+print("======")
+
+
+for i in range(1 , 4):
+    print(i)
+
+for i in range(1 , a//b):
+    print(i)
+print("======")
+
+print(a + b / 3 - 4 * 12)
+
+print("======")
+
+print((((a + b) /3) -4) * 12)
+
+print("======")
+
+c = a + b
+d = c / 3
+e = d - 4
+print(e * 12)
+
+print("======")
+
+parrot = "hello everyone"
+print(parrot)
+print(parrot[0])
+print(parrot[3])
+print(parrot[-1])
+print(parrot[0:6])
+print(parrot[:6])
+print(parrot[6:])
+print(parrot[0:6:2])
+print(parrot[0:6:3])
+print("======")
+
+
+number = "9,223,456,489,456,789,159,134"
+print(number[1::4])
+numbers = "1, 2, 3, 4, 5, 6, 7, 8, 9"
+print(numbers[0::3])
+
+print("======")
+
+string1 = "he's "
+string2 = "probably"
+print(string1 + string2)
+print("he's probably pining")
+print("Hello " * 5)
+today = "friday"
+print("day" in today)
+print("fri" in today)
+print("thur" in today)
+print("parrot" in "fjord")
+
+
+
+
+
+
+
