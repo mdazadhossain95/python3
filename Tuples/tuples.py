f3c93ac19a6659403dc5e39a323e516c23ae3688
Md Azad Hossain Tutul <58014179+mdazadhossain95@users.noreply.github.com>
2020-07-22 16:25:02 +0600

    add new class in tuples package challenge.py

--- Tuples/tuples.py
+++ Tuples/tuples.py
@@ -6,13 +6,20 @@
 welcome = "Welcome to my Nightmare", "Alice Cooper", 1975
 bad = "Bad Company" "Bad Company", 1974
 budgie = "Nightflight", "Budgie", 1981
-imelda = "More Mayhem", "Imilda May", 2011
-metallica = "Ride the Lightning", "Metallica", 1984
+imelda = "More Mayhem", "Imilda May", 2011, (1, "pulling the rug"), (2, "psycho"), (3, "mayhen"), (4, "kentish Town walth")
+# metallica = "Ride the Lightning", "Metallica", 1984
+#
+# metallica2 = ["Ride the Lightning", "Metallica", 1984]
+# print(metallica2)
+print(imelda)
 
-metallica2 = ["Ride the Lightning", "Metallica", 1984]
-print(metallica2)
-
-title, artist, year = imelda
+title, artist, year, track1, track2, track3, track4 = imelda
 print(title)
 print(artist)
 print(year)
+print(track1)
+print(track2)
+print(track3)
+print(track4)
+
+
