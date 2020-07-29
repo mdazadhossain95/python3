52a87d64995cdbe70645586b11f281ec66ebff9d
Md Azad Hossain Tutul <58014179+mdazadhossain95@users.noreply.github.com>
2020-07-29 23:54:25 +0600

    add new file sample.txt

--- FilIO/fileio.py
+++ FilIO/fileio.py
@@ -7,19 +7,26 @@ jabber = open("C:/Users/mdaza/Desktop/1.txt")
 # 	# 	print(line, end='')
 #
 # jabber.close()
-
+#--------------------------------------
 # with jabber as jabbers:
 # 	for line in jabbers:
 # 		if "MOD" in line.upper():
 # 			print(line, end='')
 # 			line = jabbers.readline()
-#
+#--------------------------------------
 # with jabber as jabbers:
 # 	lines = jabbers.readlines()
 # print(lines)
 # for line in lines[::-1]:
 # 	print(line, end='')
 
+#------------------------------------
+# with open("sample.txt") as jabber:
+# 	for line in jabber:
+# 		if "JAB" in line.upper():
+# 			print(line, end='')
+# 			line = jabber.readline()
+#------------------------------------------
 with jabber as jabbers:
 	lines = jabbers.read()
 for line in lines[::-1]:
