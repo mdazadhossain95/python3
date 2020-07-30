161b4b9b55253db6dbe99322dbd3681b418bbde7
Md Azad Hossain Tutul <58014179+mdazadhossain95@users.noreply.github.com>
2020-07-30 17:03:03 +0600

    create new package and learn turtledemo.py

---
+++ modImport/turtledemo.py
@@ -0,0 +1,12 @@
+done = "well done, you have finished your drawing"
+from turtle import *
+# import time
+
+forward(150)
+right(250)
+forward(150)
+circle(75)
+
+# time.sleep(4)
+done()
+print(done)
